4a5e8dd848e30c22385d983e4f0d9070bf84d09d
Pitamber Pradhan <146324665+pitamber92@users.noreply.github.com>
2023-11-22 08:52:05 +0000

    Notes on datatypes

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // this statement is used to follow the new standards of JavaScript Code
+
+//alert(4 + 4) //we cannot use this function in node because its in run on browsers.
+
+/*
+There are following datatypes available in JavaScript
+Number
+bigInt
+boolean
+null // it is also a standalone value, it is also a representation of empty.
+undefined // it is also a standalone value but it is use to show that value is not assigned
+symbols // it is use to define uniqueness
+
+*/
+
+console.log(typeof undefined)  // show undefined
+console.log(typeof null) // show object
